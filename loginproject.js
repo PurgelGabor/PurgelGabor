b90de80d16d753a2296e367d911a73ed18655497
expcmd0 <127344322+PurgelGabor@users.noreply.github.com>
2023-11-12 22:01:59 +0100

    Add files via upload

---
+++ loginproject.js
@@ -0,0 +1,7 @@
+document.getElementById("button").addEventListener("click", function(){
+    let Username = document.getElementById("username").value;
+    let Password = document.getElementById("password").value;
+
+    console.log(Username);
+    console.log(Password);
+})
